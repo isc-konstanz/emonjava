16c1c52bdf84d1101131a741e84af80e0ffa2ed3
Adrian Minde <Adrian_Minde@live.de>
2018-11-21 20:28:36 +0100

    Fix LGPL v3.0 licence

--- projects/openmuc-datalogger/src/main/java/org/openmuc/framework/datalogger/emoncms/EmoncmsConfig.java
+++ projects/openmuc-datalogger/src/main/java/org/openmuc/framework/datalogger/emoncms/EmoncmsConfig.java
@@ -5,16 +5,16 @@
  * For more information visit https://github.com/isc-konstanz/emonjava
  * 
  * Emonjava is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
  * Emonjava is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with emonjava.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.openmuc.framework.datalogger.emoncms;
